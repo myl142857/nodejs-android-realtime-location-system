2dae87f69cf6353eb7d188007015bde90f253239
jungsungrak@gmail.com <jungsungrak@gmail.com@a9d25e25-b9f1-0ae0-3ba7-e0d7f5020d05>
2013-08-27 10:05:31 +0000

    nodejs 서버 register 및 common등록

--- trunk/server/routes/main.js
+++ trunk/server/routes/main.js
@@ -1,52 +1,78 @@
-var gcm = require('node-gcm');
-var redis = require('redis');
-var client = redis.createClient();
+var common = require('./common/common.js')
 
-client.on('error', function(err){
-	consol.log("redis client Error " + err);
+var redis_conn = require('redis');
+var redis = redis_conn.createClient();
+
+redis.on('error', function(error){
+  consol.log("Error: "+ error);
 });
 
+
 exports.regist = function (req, res) {
-	var regId = req.body['regId'];
-	console.log (regId);
-        // 원래는 DB 에 저장하는 부분이 있어야 할 것입니다. 받아서 디비에 저장하기       
+  var data = {
+    "phoneNumber" : req.body.phoneNumber,
+    "regId" : req.body.regId
+  };
 
-	res.send("asdf");
-	res.end(); 
-} 
+  var userKey = common.makeUserKey(req.body.phoneNumber);
 
-exports.send_push = function(req, res) {
+  redis.hmset(userKey, data, function(error){
+    if (error) {
+      common.sendError(error, res);
+      common.consoleError(error);
+
+      res.end();
+    }else{
+      common.sendJson("regist ok", res);
+      res.end();
+    }
+  });
+}
+
+exports.send = function (req, res) {
+	var userKey = common.makeUserKey(req.body.phoneNumber);
+
+	redis.hmgetall(userKey, function(error, data){
+		var message = new gcm.Message();
+		var sender = new gcm.Sender('AIzaSyB8Pmbktp_SiGOWJ-XwQTvYmE9hNZCDOX8');
+		var registrationIds = [];
+
+		for(var i in data){
+			registrationIds.push(data[i].regId);
+		}
+		message.addData("hi", "hello");
+		message.collapseKey = 'demo';
+		message.delayWhileIdle = true;
+		message.timeToLive = 3;
+
+	});
 	var message = new gcm.Message();
-	var sender = new gcm.Sender('AIzaSyB8Pmbktp_SiGOWJ-XwQTvYmE9hNZCDOX8');
-	var registrationIds = [];
 
-	// Optional
-	message.addData('hi','hello');
-	message.addData('zozozo','rororo');
-	message.collapseKey = 'demo';
-	message.delayWhileIdle = true;
-	message.timeToLive = 3;
-
-// At least one required
-	registrationIds.push('APA91bGSPSEqUKDOKfdvVjmGJm0dKnOKAbX1AXpW5-NCatxMnEtkjDxeN2FGRaC5AVmAj1oOKTCEKbZ0AEq-mcETx-yC3hHK1QE1YCTRMS87Al8tZAHvhDh6b12Mm619g1EVsQ5ZsIhk2c2mQO0LjqQtRa9MtX-ZYQ');
-        // 만약 제대로 구현 됐다면 디비에서 읽어서 가져오겠지요?  
-/**
-
- * Parameters: message-literal, registrationIds-array, No. of retries, callback-function
-
- */
 	sender.send(message, registrationIds, 4, function (err, result) {
 			console.log(result);
-			res.write("Asdf");
+			res.write("done");
 			res.end();
 	});
 
-// 	sender.sendNoRetry(message, registrationIds-array, function (err, result) {
+}
 
-//     console.log(result);
+exports.get_user = function(req, res){
+  var phoneNumber = req.params.phoneNumber;
+  if(!phoneNumber){
+    common.sendError("phoneNumber is Empty", res);
+    res.end();
+    return;
+  }
+  var userKey = common.makeUserKey(phoneNumber);
+  redis.hgetall(userKey, function(error, results){
+    if(error){
+      common.sendError(error, res);
+      common.consoleError(error);
+    }else{
+      common.sendJson(results, res);
+      console.log(results);
 
-// }); // retry 없이 보내는 부분입니다. 보통은 이것을 더 많이 애용할 것 같습니다. 
-
-	
-
+      res.end();
+    }
+  });
 }
