3bff9358504437f22049bf065a7e1c0cebb4c82e
jungsungrak@gmail.com <jungsungrak@gmail.com@a9d25e25-b9f1-0ae0-3ba7-e0d7f5020d05>
2013-09-02 05:48:20 +0000

    send location 적용

--- android/gen/com/rocktest/nodejstest/R.java
+++ android/gen/com/rocktest/nodejstest/R.java
@@ -24,42 +24,41 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int display=0x7f080001;
-        public static final int options_clear=0x7f080005;
-        public static final int options_exit=0x7f080006;
-        public static final int options_register=0x7f080003;
-        public static final int options_unregister=0x7f080004;
-        public static final int webView=0x7f080000;
+        public static final int display=0x7f080000;
+        public static final int options_clear=0x7f080003;
+        public static final int options_exit=0x7f080004;
+        public static final int options_register=0x7f080001;
+        public static final int options_send_location=0x7f080005;
+        public static final int options_unregister=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int options_menu=0x7f070001;
+        public static final int options_menu=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int already_registered=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int error_config=0x7f050002;
-        public static final int gcm_deleted=0x7f050009;
-        public static final int gcm_error=0x7f050007;
-        public static final int gcm_message=0x7f050006;
-        public static final int gcm_recoverable_error=0x7f050008;
-        public static final int gcm_registered=0x7f050004;
-        public static final int gcm_unregistered=0x7f050005;
-        public static final int options_clear=0x7f050011;
-        public static final int options_exit=0x7f050012;
-        public static final int options_register=0x7f05000f;
-        public static final int options_unregister=0x7f050010;
-        public static final int server_register_error=0x7f05000d;
-        public static final int server_registered=0x7f05000b;
-        public static final int server_registering=0x7f05000a;
-        public static final int server_unregister_error=0x7f05000e;
-        public static final int server_unregistered=0x7f05000c;
+        public static final int gcm_deleted=0x7f05000a;
+        public static final int gcm_error=0x7f050008;
+        public static final int gcm_message=0x7f050007;
+        public static final int gcm_recoverable_error=0x7f050009;
+        public static final int gcm_registered=0x7f050005;
+        public static final int gcm_unregistered=0x7f050006;
+        public static final int not_registered=0x7f050004;
+        public static final int options_clear=0x7f050013;
+        public static final int options_exit=0x7f050014;
+        public static final int options_register=0x7f050010;
+        public static final int options_send_location=0x7f050012;
+        public static final int options_unregister=0x7f050011;
+        public static final int server_register_error=0x7f05000e;
+        public static final int server_registered=0x7f05000c;
+        public static final int server_registering=0x7f05000b;
+        public static final int server_unregister_error=0x7f05000f;
+        public static final int server_unregistered=0x7f05000d;
     }
     public static final class style {
         /** 
